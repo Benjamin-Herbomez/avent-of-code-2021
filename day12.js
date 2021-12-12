ae6fc080dc7dda21c078e9f9d23d98146eb7db7b
Benjamin-Herbomez <benjamin.herbomez@kry.se>
2021-12-12 10:17:19 +0100

    1 file modified

--- day12.js
+++ day12.js
@@ -1,4 +1,28 @@
-let raw = ``;
+let raw = `qi-UD
+jt-br
+wb-TF
+VO-aa
+UD-aa
+br-end
+end-HA
+qi-br
+br-HA
+UD-start
+TF-qi
+br-hf
+VO-hf
+start-qi
+end-aa
+hf-HA
+hf-UD
+aa-hf
+TF-hf
+VO-start
+wb-aa
+UD-wb
+KX-wb
+qi-VO
+br-TF`;
 
 raw = `start-A
 start-b
@@ -8,83 +32,88 @@ b-d
 A-end
 b-end`; //*/
 
-let data = raw.split('\n');
-let starts = data
-  .filter((v) => v.startsWith('start-') || v.endsWith('-start'))
-  .map((v) => v.replace('start-', '').replace('-start', ''));
-let ends = data
-  .filter((v) => v.startsWith('end-') || v.endsWith('-end'))
-  .map((v) => v.replace('end-', '').replace('-end', ''));
-let paths = data
-  .filter(
-    (v) =>
-      !v.startsWith('start-') &&
-      !v.endsWith('-start') &&
-      !v.startsWith('end-') &&
-      !v.endsWith('-end')
-  )
-  .map((v) =>
-    v.split('-').map((c) => ({ c, isBig: c.match(/[A-Z]/g) !== null }))
-  );
+let data = raw.split('\n').map((v) => v.split('-'));
+let starts = data.filter((v) => v[0] == 'start' || v[1] == 'start');
+let ends = data.filter((v) => v[0] == 'end' || v[1] == 'end');
+data = data.map((d) =>
+  d.map((c) => ({
+    c,
+    isBig: c.match(/[A-Z]/g) !== null,
+  }))
+);
 
-console.log(starts, paths, ends);
+let result = getAllPaths();
 
-let result = [];
-for (let s = 0; s < starts.length; ++s) {
-  for (let e = 0; e < ends.length; ++e) {
-    let sr = getAllPaths(starts[s], ends[e]).filter(
-      (p) => p[p.length - 1] == ends[e]
-    );
-    console.log(sr);
-    result = result.concat(sr);
-  }
+let resultWithSmall = result.filter(
+  (path) =>
+    path.find((v) => v != 'start' && v != 'end' && v.toLowerCase() == v) != null
+);
+console.log(resultWithSmall, resultWithSmall.length);
+
+function getAllPaths() {
+  let tree = buildTree('start', []);
+  return treeToArray(tree).filter((path) => path[path.length - 1] == 'end');
 }
 
-console.log(result, result.length);
-
-function getAllPaths(start, end) {
-  let tree = buildTree(start, end, []);
-  //console.log(JSON.stringify(tree));
-  return treeToArray(tree);
-}
-
-function buildTree(start, end, pile) {
+function buildTree(start, pile) {
   let r = { leaf: start, children: [] };
 
-  for (let i = 0; i < paths.length; ++i) {
-    if (paths[i][0].c == start && !pile.includes(paths[i][1].c)) {
-      if (ends.includes(paths[i][1])) {
-        r.children.push({ leaf: paths[i][1].c });
-      }
-      let pile2 = [...pile];
-      if (!paths[i][0].isBig) {
-        pile2.push(paths[i][0].c);
-      }
-      if (!paths[i][1].isBig) {
-        pile2.push(paths[i][1].c);
-      }
-      r.children.push(buildTree(paths[i][1].c, end, pile2));
+  for (let i = 0; i < data.length; ++i) {
+    const path = data[i];
+    if (path[0].c != start && path[1].c != start) {
+      continue;
     }
-    if (paths[i][1].c == start && !pile.includes(paths[i][0].c)) {
-      if (ends.includes(paths[i][0])) {
-        r.children.push({ leaf: paths[i][0].c });
-      }
-      let pile2 = [...pile];
-      if (!paths[i][0].isBig) {
-        pile2.push(paths[i][0].c);
-      }
-      if (!paths[i][1].isBig) {
-        pile2.push(paths[i][1].c);
-      }
-      r.children.push(buildTree(paths[i][0].c, end, pile2));
+    let s = path[0].c == start ? path[0] : path[1];
+    let e = path[0].c == start ? path[1] : path[0];
+    if (canAddToPath(e.c, pile)) {
+      continue;
     }
+
+    let pile2 = [...pile];
+    if (!s.isBig) {
+      let index = pile2.findIndex((v) => v.v == s.c);
+      if (index >= 0) {
+        pile2[0].nb += 1;
+      } else {
+        pile2.push({ v: s.c, nb: 1 });
+      }
+    }
+    if (!e.isBig) {
+      pile2.push(e.c);
+      let index = pile2.findIndex((v) => v.v == e.c);
+      if (index >= 0) {
+        pile2[0].nb += 1;
+      } else {
+        pile2.push({ v: e.c, nb: 1 });
+      }
+    }
+    r.children = r.children.concat(buildTree(e.c, pile2));
   }
   return r;
 }
 
 function treeToArray(tree) {
-  if (tree.children == undefined || tree.children == null) {
+  if (
+    tree.leaf == 'end' ||
+    tree.children == undefined ||
+    tree.children == null ||
+    tree.children.length === 0
+  ) {
     return [tree.leaf];
   }
-  return [tree.leaf].concat(tree.children.map((v) => treeToArray(v)).flat());
+
+  let result = [];
+  for (let i = 0; i < tree.children.length; ++i) {
+    let subR = treeToArray(tree.children[i]);
+    if (subR !== null && subR !== undefined) {
+      for (let k = 0; k < subR.length; ++k) {
+        result.push([tree.leaf].concat(subR[k]));
+      }
+    }
+  }
+  return result;
+}
+
+function canAddToPath(value, pile) {
+  return !pile.some((p) => p.v == value) || !pile.some((p) => p.nb > 1);
 }
